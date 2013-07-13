a35b187f31a1f571c4141fa0cf11e430120c818a
Leo Gau <leo.gau1@gmail.com>
2013-07-13 12:53:33 -0700

    Initial Commit

---
+++ Matchismo/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  Matchismo
+//
+//  Created by Leo Gau on 7/13/13.
+//  Copyright (c) 2013 Leo Gau. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MatchismoTests/MatchismoTests.m
@@ -0,0 +1,36 @@
+//
+//  MatchismoTests.m
+//  MatchismoTests
+//
+//  Created by Leo Gau on 7/13/13.
+//  Copyright (c) 2013 Leo Gau. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MatchismoTests : XCTestCase
+
+@end
+
+@implementation MatchismoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
